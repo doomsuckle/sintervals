488cee6d1069391eb92d74c198300ad7bbe54c32
doomsuckle <nickkypreos@gmail.com>
2014-11-25 02:35:27 -0800

    adding setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sintervals
+
+metadata = dict(name='sintervals',
+                maintainer='Nick Kypreos',
+                maintainer_email='nickkypreos@gmail.com',
+                description='',
+                license='',
+                url='',
+                version=sintervals.__version__,
+                download_url='',
+                long_description='',
+                py_modules=['clopper_pearson'])
+
+setup(**metadata)
